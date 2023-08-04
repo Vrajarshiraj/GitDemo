0010cb2e95c55af31614ca8250bf0d02e8827a81
vrajarshi <vrajarshi9873@gmail.com>
2023-08-04 14:07:20 +0530

    New Java lectures

--- src/test/LoginPackage/Merge1.java
+++ src/test/LoginPackage/Merge1.java
@@ -1,9 +1,9 @@
-package test.LoginPackage;
-
-public class Merge1 {
-    public static void main()
-    {
-        String value="No Value";
-        System.out.println(value);
-    }
-}
+//package test.LoginPackage;
+//
+//public class Merge1 {
+//    public static void main()
+//    {
+//        String value="No Value";
+//        System.out.println(value);
+//    }
+//}
