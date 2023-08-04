0826a7937988cc9f28431dd4fb2aba0b639b0e4f
vrajarshi <vrajarshi9873@gmail.com>
2023-08-04 12:07:26 +0530

    New Java lectures

---
+++ src/test/LoginPackage/JDBCconnection.java
@@ -0,0 +1,29 @@
+package test.LoginPackage;
+
+import com.mysql.cj.protocol.Resultset;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+import javax.xml.transform.Result;
+import java.sql.*;
+
+public class JDBCconnection
+{
+    public static void main(String[] args) throws SQLException {
+        WebDriverManager.chromedriver().setup();
+        String host="localhost";
+        String port="3306";
+
+        Connection conn=DriverManager.getConnection("jdbc:mysql://"+host+":"+port+"/etl","root","Vrajarshi@123");
+
+        Statement s = conn.createStatement();
+        ResultSet rs = s.executeQuery("select * from patient where patid='1'");
+
+
+        while (rs.next())
+        {
+            System.out.println(rs.getString("fname")); //returns index of 1
+            System.out.println(rs.getString("lname"));
+        }
+
+    }
+}
